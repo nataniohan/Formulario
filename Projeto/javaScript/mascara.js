a684eda7b376bd5e4d5d97d16f0b8d70466ae3a4
nataniohan <natan.teodoro@hotmail.com>
2020-10-24 19:51:31 -0300

    3 arquivos
    mudança nos 3 arquivos

--- Projeto/javaScript/mascara.js
+++ Projeto/javaScript/mascara.js
@@ -6,6 +6,6 @@ function mascara() {
     $Cep.mask("00000-000");
     $Cpf.mask("000.000.000-00");
     $dataVencimento.mask("00/0000")
-    $numeroDoCartao.mask("0000 0000 000 000")
+    $numeroDoCartao.mask("0000 0000 000 0000")
 };
 mascara();
